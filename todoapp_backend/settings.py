8a633ca3a0c8c2b9342cedb66fff1869de90c853
safaraliyevelmir <safaraliyevelmir@gmail.com>
2022-02-12 14:32:47 -0500

    smpt

--- todoapp_backend/settings.py
+++ todoapp_backend/settings.py
@@ -131,7 +131,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'safaraliyevelmir@gmail.com'
-EMAIL_HOST_PASSWORD = 'kiltcjjlwhblxtvu'
+EMAIL_HOST_PASSWORD = 'sktanevczyqctdui'
 
 # 
 CELERY_BROKER_URL = 'redis://localhost:6379'
